bdbae959092b988482178dd46bac4d982c13c821
Shir Korman <shirkorman13@gmail.com>
2022-09-15 13:57:04 +0300

    start of project

---
+++ consts.py
@@ -0,0 +1,12 @@
+import pygame.key
+
+
+SOLDIER_HEIGHT = 4
+SOLDIER_WIDTH = 2
+START_SOLDIER = (0,0)
+SCREEN_HEIGHT = 25
+SCREEN_WIDTH = 50
+UP_KEY = pygame.K_UP
+UP_KEY = pygame.K_UP
+UP_KEY = pygame.K_UP
+UP_KEY = pygame.K_UP
